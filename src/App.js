be7d025e5ce0901b59f9b4e36292630b74bb19db
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-02-22 10:11:44 -0700

    button gets styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React/*, {useState}*/ from 'react';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './page.css';
 
 export default function App() {
     return (
-      <div>
-        <h1>Welcome to my app!</h1>
-        <MyButton />
-      </div>
+        <div className="App">
+            <div>
+                <h1>Proof Of Concept</h1>
+                <MyButton/>
+            </div>
+            <br/>
+            <a href={"http://localhost:5000/api/upload"}>Click here to upload images</a>
+        </div>
     );
 }
 
 
 function MyButton() {
+    useEffect( () => {
+        axios.get("/api/garbage").then((response) => {
+            console.log(response.data);
+        });
+    }, []);
 
     function handleClick() {
-        alert('holy shit!');
+        axios.post("/api/send", {
+            message: "Hello Server!",
+        }).then((response) => {}).catch((error) => {
+            console.log(error);
+        });
     }
 
     return (
-        <button  onClick={handleClick}>
-            I don't change colors!
+        <button style={{ backgroundColor: 'black', color: '#69cf97', fontFamily: 'Arial' }} onClick={handleClick}>
+            I send data to the server
         </button>
     );
 }
